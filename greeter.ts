73597fb7f62ebc1a0c9adcd81717bbff3a095187
Londeka Zikalala <londekazikalala@gmail.com>
2024-04-15 16:10:19 +0200

    New Greeter tests fixed

--- greeter.ts
+++ greeter.ts
@@ -1,32 +1,35 @@
 import { language } from "./language";
 import GreetIn from "./greetin";
-import GreetInXhosa from "./greetInXhosa";
+import MapUserGreetCounter from "./IUserGreetCounter";
 
-
-let theGreetInMap: Map<language, GreetIn> = new Map()
-
-//Map object for Xhosa
-
-theGreetInMap.set(language.xhosa, new GreetInXhosa());
-
-//Get the Xhosa greeting
-let greeting = theGreetInMap.get(language.xhosa);
-console.log(greeting.greet('Lindani'));
-
-// the Greeter class
 
 export default class Greeter {
     //A map that has a languages enum as a key and a GreetIn interface instance as a value
     private greetLanguages: Map<language, GreetIn>;
-    constructor(greetLanguages: Map<language, GreetIn>) {
+    private userGreetCounter: MapUserGreetCounter;
+
+    constructor(greetLanguages: Map<language, GreetIn>, userGreetCounter: MapUserGreetCounter) {
         this.greetLanguages = greetLanguages;
+        this.userGreetCounter = userGreetCounter;
     }
 
     greet(name: string, chosenLanguage: language) {
-        let greetIn = this.greetLanguages.get(chosenLanguage)
+        let greetIn = this.greetLanguages.get(chosenLanguage);
+        // keep track of how many users has been greeted
+        this.userGreetCounter.countGreet(name);
         if (greetIn) {
-            return greetIn.greet(name)
+            return greetIn.greet(name);
         }
-        return ""
+        return "";
+    }
+
+       // call the greetCounter on the userGreetCounter
+       public get greetCounter(): number {
+        return this.userGreetCounter.greetCounter;
+    }
+    
+    // call the userGreetCount on the userGreetCounter
+    userGreetCount(firstName: string): number {
+        return this.userGreetCounter.userGreetCount(firstName);
     }
 }
--- test/test.greeter.ts
+++ test/test.greeter.ts
@@ -5,15 +5,17 @@ import Greeter from "../greeter";
 import GreetIn from "../greetin";
 import assert from "assert";
 import { language } from "../language";
+import MapUserGreetCounter from "../IUserGreetCounter";
 
 describe('The Greeter function', function () {
     it('should greet a user in French, Xhosa and Afrikaans', function () {
         let greetMap = new Map<language, GreetIn>();
+        let mapUserGreetCounter = new MapUserGreetCounter()
         greetMap.set(language.french, new GreetInFrench());
         greetMap.set(language.xhosa, new GreetInXhosa());
         greetMap.set(language.afr, new GreetInAfrikaans());
 
-        let greeter = new Greeter(greetMap);
+        let greeter = new Greeter(greetMap, mapUserGreetCounter);
 
         assert.equal(greeter.greet("Bob", language.french), 'Bonjour, Bob');
         assert.equal(greeter.greet("Bob", language.xhosa), 'Molo, Bob');
@@ -22,11 +24,42 @@ describe('The Greeter function', function () {
 
     it('should return an empty string when a language is not recognised', function () {
         let greetMap = new Map<language, GreetIn>();
+        let mapUserGreetCounter = new MapUserGreetCounter()
+
         greetMap.set(language.french, new GreetInFrench());
         greetMap.set(language.xhosa, new GreetInXhosa());
-        let greeter = new Greeter(greetMap);
+        let greeter = new Greeter(greetMap, mapUserGreetCounter);
 
         assert.equal(greeter.greet("Bob", language.frech), '');
 
     });
+    it('should count the number of greeted users', function () {
+        let greetMap = new Map<language, GreetIn>();
+        let mapUserGreetCounter = new MapUserGreetCounter()
+
+        greetMap.set(language.french, new GreetInFrench());
+        greetMap.set(language.xhosa, new GreetInXhosa());
+        let greeter = new Greeter(greetMap, mapUserGreetCounter);
+        greeter.greet("Bob", language.french);
+        greeter.greet("Londeka", language.xhosa);
+        greeter.greet("Andre", language.xhosa);
+
+        assert.equal(greeter.greetCounter, 3);
+
+    })
+    it('should increment the counter if a user greets multiple times', function () {
+        let greetMap = new Map<language, GreetIn>();
+        let mapUserGreetCounter = new MapUserGreetCounter()
+
+        greetMap.set(language.french, new GreetInFrench());
+        greetMap.set(language.xhosa, new GreetInXhosa());
+        let greeter = new Greeter(greetMap, mapUserGreetCounter);
+        greeter.greet("Bob", language.french);
+        greeter.greet("Londeka", language.xhosa);
+        greeter.greet("Londeka", language.afr);
+
+        assert.equal(greeter.greetCounter, 2);
+
+    })
+
 });
